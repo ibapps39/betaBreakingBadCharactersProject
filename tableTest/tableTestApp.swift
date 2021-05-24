166e34679a4fd1b28383e3ebf0d9d8e605d9d289
Ian Brown <ibapps39@gmail.com>
2021-05-24 14:53:17 -0600

    Initial Commit

---
+++ tableTest/tableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  tableTestApp.swift
+//  tableTest
+//
+//  Created by Ian Brown on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct tableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
