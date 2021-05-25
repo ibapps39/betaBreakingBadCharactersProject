325fd4ff88c89c99129ca6a86ef1f110cc5dd106
Ian Brown <ibapps39@gmail.com>
2021-05-25 17:01:18 -0600

    adding AlamoFire

--- tableTest/tableTestApp.swift
+++ tableTest/tableTestApp.swift
@@ -16,3 +16,9 @@ struct tableTestApp: App {
         }
     }
 }
+
+struct tableTestApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
